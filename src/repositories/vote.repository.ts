18d4368cd328a01edb7e3f6a8fb3bfb4ebd558d1
rezonmain <adiazgomez101@gmail.com>
2023-12-15 23:23:59 -0800

    feat(voteRepo): remove cl

--- src/repositories/vote.repository.ts
+++ src/repositories/vote.repository.ts
@@ -1,7 +1,6 @@
 import { getLocalDBInstance } from "@/lib/db";
 
 const createNewVote = (id: string, option: string) => {
-  console.log("createNewVote", id, option);
   const db = getLocalDBInstance(id);
   const stmt = db.prepare("INSERT INTO vote (optionId) values ($optionId)");
   stmt.run({ $optionId: option });
