cd76b7ff664ea8ede3850f878005252725514efa
rezonmain <adiazgomez101@gmail.com>
2023-12-15 23:30:01 -0800

    fix(index): import

--- index.tsx
+++ index.tsx
@@ -2,8 +2,8 @@ import "typed-html";
 import { Elysia } from "elysia";
 import { html } from "@elysiajs/html";
 import { ip } from "elysia-ip";
-import { runMigrations } from "./src/lib/db";
 import { home, poll, result } from "@/controllers";
+import { runMigrations } from "@/lib/db";
 
 runMigrations();
 new Elysia().use(ip()).use(html()).use(home).use(poll).use(result).listen(3000);
